b0c73b9d6af6ef74dc04cf77a618288514c6a35b
avinabsaha <avinab.saha@utexas.edu>
2022-10-27 09:59:44 -0500

    add resnet ms net

--- networks/resnet.py
+++ networks/resnet.py
@@ -7,8 +7,16 @@ except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 from .resnest import resnest50, resnest101
+from typing import Type, Callable, Tuple, Optional, Set, List, Union
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+import torch
+import torch.nn as nn
+
+from timm.models.efficientnet_blocks import SqueezeExcite, DepthwiseSeparableConv
+from timm.models.layers import drop_path, trunc_normal_, Mlp, DropPath
+
+
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet50_multistage' 'resnet101', 'resnet101_multistage'
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
            'wide_resnet50_2', 'wide_resnet101_2', 'model_dict']
 
@@ -126,6 +134,45 @@ class Bottleneck(nn.Module):
         return out
 
 
+
+class MultiStageBlock(nn.Module):
+
+
+    def __init__(self, inplanes, norm_layer=None):
+        super(MultiStageBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        self.conv1 = nn.Conv2d(inplanes, inplanes//2, kernel_size=1)
+        self.bn1 = norm_layer(inplanes//2)
+        self.conv2 = nn.Conv2d(inplanes//2, inplanes//2, kernel_size=3, padding = 1)
+        self.bn2 = norm_layer(inplanes//2)
+        self.conv3 = nn.Conv2d(inplanes//2, inplanes, kernel_size=1)
+        self.bn3 = norm_layer(inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        
+
+    def forward(self, x):
+        
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+
+        return out
+
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, width=1, in_channel=3, zero_init_residual=False,
@@ -222,9 +269,143 @@ class ResNet(nn.Module):
 
         return x
 
+class ResNet_MS(nn.Module):
+
+    def __init__(self, block, layers, width=1, in_channel=3, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet_MS, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+        self.total_feats = 0
+        self.inplanes = max(int(64 * width), 64)
+        self.base = int(64 * width)
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(in_channel, self.inplanes, kernel_size=7, stride=2, padding=3,bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.total_feats += self.inplanes
+        self.ms_layer0 = MultiStageBlock(self.inplanes, norm_layer=None)
+
+        self.layer1 = self._make_layer(block, self.base, layers[0])
+        
+        self.total_feats += self.inplanes
+        self.ms_layer1 = MultiStageBlock(self.inplanes, norm_layer=None)
+        self.layer2 = self._make_layer(block, self.base * 2, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+
+        self.total_feats += self.inplanes
+        self.ms_layer2 = MultiStageBlock(self.inplanes, norm_layer=None)
+        self.layer3 = self._make_layer(block, self.base * 4, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+
+        self.total_feats += self.inplanes
+        self.ms_layer3 = MultiStageBlock(self.inplanes, norm_layer=None)
+        self.layer4 = self._make_layer(block, self.base * 8, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.total_feats += self.inplanes
+
+        self.fc_out = nn.Linear(self.total_feats,self.inplanes)
+
+        
+
+        # comment out fc layer for unsupervised learning, will have another head
+        # self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        y0 = self.ms_layer0(x)
+        
+
+        x = self.layer1(x)
+        y1 = self.ms_layer1(x)
+
+        x = self.layer2(x)
+        y2 = self.ms_layer2(x)
+
+        x = self.layer3(x)
+        y3 = self.ms_layer3(x)
+
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        x = torch.cat([y0,y1,y2,y3,x],axis=1)
+        x = self.fc_out(x)
+
+        return x
+
 
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
     model = ResNet(block, layers, **kwargs)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls[arch],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
+
+
+def _resnet_multistage(arch, block, layers, pretrained, progress, **kwargs):
+    model = ResNet_MS(block, layers, **kwargs)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls[arch],
                                               progress=progress)
@@ -264,6 +445,16 @@ def resnet50(pretrained=False, progress=True, **kwargs):
     return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
                    **kwargs)
 
+def resnet50_multistage(pretrained=False, progress=True, **kwargs):
+    r"""ResNet-50 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet_multistage('resnet50_multistage', Bottleneck, [3, 4, 6, 3], pretrained, progress,
+                   **kwargs)
+
 
 def resnet101(pretrained=False, progress=True, **kwargs):
     r"""ResNet-101 model from
@@ -273,6 +464,16 @@ def resnet101(pretrained=False, progress=True, **kwargs):
         progress (bool): If True, displays a progress bar of the download to stderr
     """
     return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], pretrained, progress,
+                   **kwargs)
+
+def resnet101_multistage(pretrained=False, progress=True, **kwargs):
+    r"""ResNet-101 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet_multistage('resnet101', Bottleneck, [3, 4, 23, 3], pretrained, progress,
                    **kwargs)
 
 
@@ -410,6 +611,9 @@ def wide_resnet101_2(pretrained=False, progress=True, **kwargs):
                    pretrained, progress, **kwargs)
 
 
+
+
+
 model_dict = {
     'resnet50': resnet50,
     'resnet101': resnet101,
@@ -423,12 +627,14 @@ model_dict = {
     'resnext152v3': resnext152_64x4d,
     'resnest50': resnest50,
     'resnest101': resnest101,
+    'resnet50_multistage' : resnet50_multistage,
+    'resnet101_multistage' : resnet101_multistage
 }
 
 
 if __name__ == '__main__':
 
-    model = resnet50(width=0.5)
-    data = torch.randn(2, 3, 224, 224)
+    model = resnet101_multistage(width=1)
+    data = torch.randn(1, 3, 224, 224)
     out = model(data)
     print(out.shape)
